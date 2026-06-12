9a7b12bd55b8c650d6b93bad62a03adc25e61e9e
OBED <uwakweobed89@gmail.com>
2026-06-12 16:12:27 +0100

    Initial project structure - app, infrastructure, sonarqube config

--- app/src/index.js
+++ app/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.disable('x-powered-by');
 const PORT = process.env.PORT || 8080;
 // Middleware to parse JSON bodies
 app.use(express.json());
